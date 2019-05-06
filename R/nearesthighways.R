7154080f7813ad0b981084ea849f93d05c9ca1b2
farinoush sharifi <farinoushsharifi@tamu.edu>
2019-05-05 23:49:16 -0500

    added match link function

--- R/nearesthighways.R
+++ R/nearesthighways.R
@@ -1,63 +1,66 @@
 
 
-source_osmdata <- function (LatList, LongList, timeseq, resolution, type, osmlink2, k) {
+match_highway <- function (LatList, LongList, k,
+                           boxcuts = get_boxes(LatList,LongList,timeseq,resolution,offLong,offLat)$boxtable$boxcuts,
+                           boxlist = get_boxes(LatList,LongList,timeseq,resolution,offLong,offLat)$boxlist,
+                           resolution = 10, offLong = 0.001, offLat = 0.001,
+                           osmlink = "https://api.openstreetmap.org/api/0.6/") {
 
-  if (missing(osmlink)) {
-
-    osmlink <- "https://api.openstreetmap.org/api/0.6/"
+  if (length(LatList)!=length(LongList)) {
+    stop("Latitude and longitude lists do not have the same length")
   }
 
-  api <- osmsource_api(osmlink)
+  if (length(LatList)!=length(boxcuts)) {
+    stop("Latitude and bounding box cut lists do not have the same length")
+  }
 
-  boxout <- get_boxes(LatList,LongList,timeseq,resolution)
-  boxlist <- boxout$boxlist
-  boxtable <- boxout$boxtable
-  LatList <- boxtable$Latitude
-  LongList <- boxtable$Longitude
-  timeseq <- boxtable$Time
+  if ((k!= round(k))|(k<0)|(k==0)){
+    stop("k is not acceptable, please enter a positive integer")
+  }
 
-  if (type == "Regional") {
+  api <- osmar::osmsource_api(osmlink)
+  highway <-  matrix(0,nrow=length(LatList),ncol=k)
 
-    for (i in 1:ncol(boxlist)) {
+  for (i in 1:ncol(boxlist)) {
 
       mapbox <- osmar::corner_bbox(boxlist[1,i],boxlist[2,i],boxlist[3,i],boxlist[4,i])
       location <- osmar::get_osm(mapbox, source = api)
-      # hways <- subset(location, way_ids = osmar::find(location, osmar::way(osmar::tags(k == "highway"))))
-      # hways <- osmar::find(hways, osmar::way(osmar::tags(k == "name")))
-      # hways <- osmar::find_down(location, osmar::way(hways))
-      # hways <- subset(location, ids = hways)
-      #
-      # nodes_coordtbl <- data.frame(hways$nodes$attrs)
 
-      ats <- location$nodes$attrs
-      # distmat <- data.frame(geosphere::distm(cbind(ats$lon, ats$lat),
-      #                       cbind(LongList[boxtable$boxcuts==i], LatList[boxtable$boxcuts==i]))/1000,
-      #                       row.names = ats$id)
-      # colnames(distmat) <- boxtable$PointNumber[boxtable$boxcuts==i]
+      hwaysdata <- subset(location, way_ids = osmar::find(location, osmar::way(osmar::tags(k == "highway"))))
+      hways <- osmar::find(hwaysdata, osmar::way(osmar::tags(k == "name")))
+      hways <- osmar::find_down(location, osmar::way(hways))
+      hwaysdata <- subset(location, ids = hways)
 
-      nn2(cbind(ats$lon, ats$lat),
-          cbind(LongList[boxtable$boxcuts==i], LatList[boxtable$boxcuts==i]),k=15)
+      hwaynodetbl <- hwaysdata$nodes$attrs
 
-      for (j in 1:ncol(distmat)) {
-        hwylist <- list(ats[order(distmat[,j]),1][1:k])
-        boxtable$Highways[boxtable$boxcuts==i][j] <- hwylist
+
+      nnmat <- RANN::nn2(cbind(hwaynodetbl$lon, hwaynodetbl$lat),
+                         cbind(LongList[boxcuts==i], LatList[boxcuts==i]),k)
+      nnmat <- matrix(hwaynodetbl[matrix(nnmat$nn.idx,ncol = k),1],ncol = k)
+
+      for(j in 1:nrow(nnmat)){
+        hwaylist <- unique(osmar::find_up(hwaysdata, node(nnmat[j,]))$way_ids)
+        count <- length(hwaylist)
+        if (count>k) {
+          count <- k
+        }
+        highway[boxcuts==i,][j,1:k] <- hwaylist[1:k]
       }
-
-    }
-
   }
 
 
-} else if (type == "Global") {
+  highway <- cbind("PointNumber"=seq(1,length(LatList)),as.data.frame(highway))
 
-  if (boxlist!=NULL) {
-    stop("For the Global Analysis, only Latitudes and Longitudes should be provided, Not boxlists")
-  }
-  if ((LatList==NULL)|(LongList==NULL)) {
-    stop("For the Global Analysis, Latitude and Longitude lists should be provided")
-  }
+  highway <- highway%>%mutate(k1lag = lag(V1), k1lead = lead(V1))
+  PointID <- highway[(highway$V1!=highway$k1lag)&(highway$k1lag==highway$k1lead),]$PointNumber
+  PointID <- PointID[!is.na(PointID)]
 
-} else {
-  stop("Type of analysis is not provided")
+  highway$V1[PointID] <- highway$k1lag[PointID]
+
+  highway <- list(highway$V1)
+
+  return("HighwayLinkID"=highway)
 }
-}
+
+
+
